266f00a2f4b8d3dc11e6b9f83c7a8bfa8c0ba53b
sanchopanda <ivanovaa1992@gmail.com>
2020-06-03 16:07:04 +0300

    add webp for gulp

--- gulp/tasks/imageMinify.js
+++ gulp/tasks/imageMinify.js
@@ -2,11 +2,11 @@ const gulp = require('gulp')
 const imagemin = require('gulp-imagemin')
 
 module.exports = function imageMinify() {
-  return gulp.src('src/img/*.{gif,png,jpg,svg,webp}')
+  return gulp.src('src/img/*.{gif,png,jpg,svg}')
     .pipe(imagemin([
       imagemin.gifsicle({ interlaced: true }),
       imagemin.mozjpeg({
-        quality: 75,
+        quality: 80,
         progressive: true
       }),
       imagemin.optipng({ optimizationLevel: 5 }),
@@ -19,4 +19,5 @@ module.exports = function imageMinify() {
     ]))
     .pipe(gulp.dest('build/img'))
 }
+
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const gulp = require('gulp')
 
 const serve = require('./gulp/tasks/serve')
-const pug2html = require('./gulp/tasks/pug2html')
+const twig2html = require('./gulp/tasks/twig2html')
 const styles = require('./gulp/tasks/styles')
 const script = require('./gulp/tasks/script')
 const fonts = require('./gulp/tasks/fonts')
 const imageMinify = require('./gulp/tasks/imageMinify')
+const imageWebp = require('./gulp/tasks/imageWebp')
 const clean = require('./gulp/tasks/clean')
 const lighthouse = require('./gulp/tasks/lighthouse')
 const svgSprite = require('./gulp/tasks/svgSprite')
+
 
 function setMode(isProduction = false) {
   return cb => {
@@ -17,7 +19,7 @@ function setMode(isProduction = false) {
   }
 }
 
-const dev = gulp.parallel(pug2html, styles, script, fonts, imageMinify, svgSprite)
+const dev = gulp.parallel(twig2html, styles, script, fonts, imageMinify, imageWebp, svgSprite)
 
 const build = gulp.series(clean, dev)
 
