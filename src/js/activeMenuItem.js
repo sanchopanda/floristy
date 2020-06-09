015d9b2ffdf162246d762578bf86e0cc8d6065d1
sanchopanda <ivanovaa1992@gmail.com>
2020-06-09 18:09:51 +0300

    fix active menu

--- src/js/activeMenuItem.js
+++ src/js/activeMenuItem.js
@@ -4,7 +4,7 @@ const menu = document.querySelectorAll(`.menu__item--header`);
 
 export function activeMenuItem() {
     menu.forEach((item) => {
-        if ("/" + item.querySelector(`.menu__link`).getAttribute('href') == window.location.pathname) {
+        if (window.location.pathname.includes(item.querySelector(`.menu__link`).getAttribute('href'))) {
             item.classList.add('menu__item--active');
         };
     });
