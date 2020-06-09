01f415b2f93140dfd3853165a990203190d40402
sanchopanda <ivanovaa1992@gmail.com>
2020-06-09 17:33:07 +0300

    gh-page

--- gulp/tasks/imageWebp.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp')
-const webp = require('gulp-webp');
-
-module.exports = function imagewebp() {
-  return gulp.src('src/img/**/*.{png,jpg}')
-    .pipe(webp())
-    .pipe(gulp.dest('docs/img'))
-}
