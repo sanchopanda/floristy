eeed5701e5cefe51806b63d9a2bb27be93b8556c
sanchopanda <ivanovaa1992@gmail.com>
2020-06-10 19:03:28 +0300

    fix picture for safari

--- src/js/activeMenuItem.js
+++ src/js/activeMenuItem.js
@@ -6,7 +6,6 @@ export function activeMenuItem() {
     menu.forEach((item) => {
         if (window.location.pathname.includes(item.querySelector(`.menu__link`).getAttribute('href'))) {
             item.classList.add('menu__item--active');
-            console.log(window.location.pathname);
         };
     });
 }
